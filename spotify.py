6fc38fb454eebaaa232cf23baab387712439943e
Zack <zacksfeverdream@gmail.com>
2025-05-24 22:25:26 -0400

    Added artist and album functions

--- spotify.py
+++ spotify.py
@@ -5,6 +5,7 @@ from requests import post, get
 import json
 
 # https://www.youtube.com/watch?v=WAmEZBEeNmg&t=750s
+# https://developer.spotify.com/documentation/web-api
 
 load_dotenv()
 
@@ -40,8 +41,42 @@ def search_for_artist(token, artist_name):
     query_url = url + query
     result = get(query_url, headers=headers)
     json_result = json.loads(result.content)['artists']['items']
+    result_dict = {}
+    i = 0
     for result in json_result:
-        print(result)
+        result_dict[i] = result
+        i += 1
+
+    return result_dict
+
+def pull_artist_by_id(token, artist_id):
+    url = f'https://api.spotify.com/v1/artists/{artist_id}'
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+
+    return json_result
+
+
+def pull_artist_albums_by_id(token, artist_id):
+    url = f'https://api.spotify.com/v1/artists/{artist_id}/albums'
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    
+    return json_result
+
+
+def pull_album_tracks_by_id(token, album_id):
+    url = f'https://api.spotify.com/v1/albums/{album_id}/tracks'
+    headers = get_auth_header(token)
+
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    
+    return json_result
 
 
 
@@ -49,9 +84,20 @@ token = get_token()
 print(token)
 
 
-search_for_artist(token, 'murkage dave')
+# murkage_dave_dict = search_for_artist(token, 'murkage dave')
+# with open('murkage_dave_query.json', 'w', encoding='utf-8') as f:
+#     json.dump(murkage_dave_dict, f, ensure_ascii=False, indent=4)
+
+thursday_dict = search_for_artist(token, 'thursday')
+with open('thursday_query.json', 'w', encoding='utf-8') as f:
+    json.dump(thursday_dict, f, ensure_ascii=False, indent=4)
 
 
+thursday_album_dict = pull_artist_albums_by_id(token, '61awhbNK16ku1uQyXRsQj5')
+with open('thursday_albums.json', 'w', encoding='utf-8') as f:
+    json.dump(thursday_album_dict, f, ensure_ascii=False, indent=4)
 
 
-
+full_collapse_live_dict = pull_album_tracks_by_id(token, '6mrm3oPwDEXoxdX6WPNrs2')
+with open('full_collapse_live.json', 'w', encoding='utf-8') as f:
+    json.dump(full_collapse_live_dict, f, ensure_ascii=False, indent=4)
